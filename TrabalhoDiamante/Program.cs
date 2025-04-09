d745f19b598abb577facb7b92e382289dd8e64e4
Dani-linus <dns40625@gmail.com>
2025-04-08 21:50:24 -0300

    Add project files.

---
+++ TrabalhoDiamante/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TrabalhoDiamante
+{
+    internal class Program
+    {
+        //O programa deve solicitar ao usuário um número inteiro.
+        //O número deve ser ímpar. Caso contrário, o programa deve informar que a entrada é inválida e solicitar um novo número.
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("        Diamante de X      ");
+                Console.WriteLine("----------------------------");
+
+                Console.Write("Digite um número inteiro parar gerar o diamante: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero % 2 == 1)
+                {
+                    Console.WriteLine($"{numero} é um número impar.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($" O {numero} não é um número impar.");
+                    continue;
+                }
+            }
+        }
+    }
+}
